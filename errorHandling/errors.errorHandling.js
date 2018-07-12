f684eb10a8dfced9eb27a14462aac12802915553
Justin <justhays124@gmail.com>
2018-07-11 17:48:42 -0700

    add error handling for deity not found

--- errorHandling/errors.errorHandling.js
+++ errorHandling/errors.errorHandling.js
@@ -2,6 +2,7 @@ function processErrorMessage(err) {
   
     if (err.message) {
       switch (err.message) {
+        case 'deitynotfound': return { status: 404, message: 'The ID provided does not match any deity on file' }
   
         default:
           return { status: 500, message: 'An internal server error has occurred.' }
