29cc64ca036d455c99ae52cf0569f7ca959b7194
Justin <justhays124@gmail.com>
2018-07-11 17:47:41 -0700

    added viewOne

--- controllers/deities.controller.js
+++ controllers/deities.controller.js
@@ -4,10 +4,19 @@ const { Deities } = require('../models')
 function viewAll(req, res, next) {
     Deities.viewAll()
         .then(deities => {
-            res.status(200).send(deities)
+            return res.status(200).send(deities)
+        })
+        .catch(err => next(err))
+}
+
+function viewOne(id) {
+    Deities.viewOne(id)
+        .then(deity => {
+            if (!deity) throw new Error('deitynotfound')
+            return res.status(200).send(deity)
         })
         .catch(err => next(err))
 }
 
 
-module.exports = { viewAll }
+module.exports = { viewAll, viewOne }
